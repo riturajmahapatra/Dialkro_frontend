18b11fd655c8fdb98908ab8b4998a7f7fa41b201
profabhishekjha <jhaabhishek910@gmail.com>
2023-10-14 10:34:15 +0530

    dark mode removed

--- src/components/modals/RegisterModal.jsx
+++ src/components/modals/RegisterModal.jsx
@@ -39,10 +39,10 @@ function RegisterModal({ onClose, onLoginClick }) {
       className="fixed bottom-0 left-0 right-0 top-0 z-10 flex h-screen max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden bg-gray-300/70 p-4  md:inset-0">
       <div className="relative max-h-full w-full max-w-md">
         {/* Modal content */}
-        <div className="relative z-50 rounded-lg bg-white shadow dark:bg-gray-700">
+        <div className="relative z-50 rounded-lg bg-gray-700 shadow">
           <button
             type="button"
-            className="absolute right-2.5 top-3 ml-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-gray-500 bg-transparent text-sm text-black  dark:hover:bg-gray-600 dark:hover:text-black"
+            className="absolute right-2.5 top-3 ml-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-gray-500 bg-transparent text-sm text-black  hover:bg-gray-600 hover:text-black"
             data-modal-hide="authentication-modal"
             onClick={() => {
               onClose() // Call the onClose function to close the modal
@@ -51,15 +51,11 @@ function RegisterModal({ onClose, onLoginClick }) {
             <span className="sr-only">Close modal</span>
           </button>
           <div className="px-6 py-6 lg:px-8">
-            <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
-              Sign up to Dialkro
-            </h3>
+            <h3 className="mb-4 text-xl font-medium  text-white">Sign up to Dialkro</h3>
             <form onSubmit={handleSubmit} className="space-y-6" action="#">
               {/* Form inputs and submit button */}
               <div>
-                <label
-                  htmlFor="email"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+                <label htmlFor="email" className="mb-2 block text-sm font-medium  text-white">
                   Your email
                 </label>
                 <input
@@ -67,15 +63,13 @@ function RegisterModal({ onClose, onLoginClick }) {
                   type="email"
                   name="email"
                   id="email"
-                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
+                  className="block w-full rounded-lg border  border-gray-500 bg-gray-600  p-2.5 text-sm text-white placeholder-gray-400 focus:border-blue-500 focus:ring-blue-500"
                   placeholder="name@company.com"
                   required
                 />
               </div>
               <div>
-                <label
-                  htmlFor="text"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+                <label htmlFor="text" className="mb-2 block text-sm font-medium  text-white">
                   Name
                 </label>
                 <input
@@ -83,15 +77,13 @@ function RegisterModal({ onClose, onLoginClick }) {
                   type="text"
                   name="name"
                   id="name"
-                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
+                  className="block w-full rounded-lg border  border-gray-500 bg-gray-600  p-2.5 text-sm text-white placeholder-gray-400 focus:border-blue-500 focus:ring-blue-500"
                   placeholder="Your name"
                   required
                 />
               </div>
               <div>
-                <label
-                  htmlFor="password"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+                <label htmlFor="password" className="mb-2 block text-sm font-medium  text-white">
                   Your password
                 </label>
                 <input
@@ -100,14 +92,12 @@ function RegisterModal({ onClose, onLoginClick }) {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
+                  className="block w-full rounded-lg border  border-gray-500 bg-gray-600  p-2.5 text-sm text-white placeholder-gray-400 focus:border-blue-500 focus:ring-blue-500"
                   required
                 />
               </div>
               <div>
-                <label
-                  htmlFor="password"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+                <label htmlFor="password" className="mb-2 block text-sm font-medium  text-white">
                   Re-enter your password
                 </label>
                 <input
@@ -116,7 +106,7 @@ function RegisterModal({ onClose, onLoginClick }) {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
+                  className="block w-full rounded-lg border  border-gray-500 bg-gray-600  p-2.5 text-sm text-white placeholder-gray-400 focus:border-blue-500 focus:ring-blue-500"
                   required
                 />
               </div>
@@ -127,28 +117,23 @@ function RegisterModal({ onClose, onLoginClick }) {
                       id="remember"
                       type="checkbox"
                       value=""
-                      className="focus:ring-3 h-4 w-4 rounded border border-gray-300 bg-gray-50 focus:ring-blue-300 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800"
+                      className="focus:ring-3 h-4 w-4 rounded border  border-gray-500 bg-gray-600 ring-offset-gray-800  focus:ring-blue-600 focus:ring-offset-gray-800"
                       required
                     />
                   </div>
-                  <label
-                    htmlFor="remember"
-                    className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  <label htmlFor="remember" className="ml-2 text-sm font-medium  text-gray-300">
                     Remember me
                   </label>
                 </div>
               </div>
               <button
                 type="submit"
-                className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                className="w-full rounded-lg  bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-800">
                 Register your account
               </button>
-              <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+              <div className="text-sm font-medium text-white">
                 Already have an account?{' '}
-                <a
-                  href="#"
-                  className="text-blue-700 hover:underline dark:text-blue-500"
-                  onClick={handleLoginClick}>
+                <a href="#" className=" text-blue-500 hover:underline" onClick={handleLoginClick}>
                   Login
                 </a>
               </div>
